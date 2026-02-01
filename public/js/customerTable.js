15c8e69612b48fbad06e2772f89c4f982fbc731d
Romii646 <cortinacort@gmail.com>
2026-02-01 11:35:17 -0500

    Modifiy the formLoader for the customer and rental page.

--- public/js/customerTable.js
+++ public/js/customerTable.js
@@ -1,11 +1,22 @@
-
 window.customerTable = {
-    'customer': [{
-        "customer_id" : "Customer ID",
-        "first_name" : "First Name",
-        "last_name" : "Last Name",
-        "email" : "Email",
-        "Bnumber" : "B Number",
-        "registration_date" : "Registation Date"
-    }],
+  customer: [
+    {
+      customer_id: "Customer ID",
+      first_name: "First Name",
+      last_name: "Last Name",
+      email: "Email",
+      Bnumber: "B Number",
+      registration_date: "Registation Date",
+    },
+  ],
+  overdueRentals: [
+    {
+      rental_id: "Rental ID",
+      customer_id: "Customer ID",
+      object_id: "Item ID",
+      start_date: "Start Date",
+      expected_return_date: "Expected Return Date",
+      status: ["overdue"],
+    },
+  ],
 };
--- public/js/session-data.js
+++ public/js/session-data.js
@@ -1,28 +1,28 @@
 const sessionData = () => {
-    fetch('../app/Routes/Router.php?router=grabSession',{
-        method : 'POST'
+  fetch("../app/Routes/Router.php?router=grabSession", {
+    method: "POST",
+  })
+    .then((response) => {
+      if (!response.ok) {
+        window.alert("Session expired, please login again");
+        window.location.href = "./login-page.html";
+        return;
+      }
+      return response.json();
     })
-    .then(response => {
-        if (!response.ok) {
-            window.alert('Session expired, please login again');
-            window.location.href = './LoginPage.html';
-            return;
+    .then((data) => {
+      if (data && data.employeeID) {
+        const el = document.getElementById("welcome");
+        if (el) {
+          el.innerHTML = `Welcome ${data.firstName} <br> Role: ${data.employeeType}`;
         }
-        return response.json();
+      }
     })
-    .then(data => {
-        if (data && data.employeeID) {
-            const el = document.getElementById('welcome');
-            if (el) {
-                el.innerHTML = `Welcome ${data.firstName} <br> Role: ${data.employeeType}`;
-            }
-        }
-    })
-    .catch(error => {
-        console.error('Error fetching session data:', error.message);
-        window.alert('Error fetching session data: ' + error.message);
-        window.location.href = './LoginPage.html';
+    .catch((error) => {
+      console.error("Error fetching session data:", error.message);
+      window.alert("Error fetching session data: " + error.message);
+      window.location.href = "./LoginPage.html";
     });
-}
+};
 
-addEventListener('DOMContentLoaded', sessionData);
+addEventListener("DOMContentLoaded", sessionData);
